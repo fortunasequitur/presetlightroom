76b6a10ca1a2cd420eab0edd5c6a68f76ce3cf37
Wisnu Dinar Pangestu <canvatron@gmail.com>
2025-06-04 14:29:18 +0700

    Update: install Meta Pixel (Facebook Pixel) with correct ID

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,14 +60,14 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', 'YOUR_PIXEL_ID');
+              fbq('init', '2211990365917167');
               fbq('track', 'PageView');
             `,
           }}
         />
         <noscript>
           <img height="1" width="1" style={{display:'none'}}
-               src="https://www.facebook.com/tr?id=YOUR_PIXEL_ID&ev=PageView&noscript=1" />
+               src="https://www.facebook.com/tr?id=2211990365917167&ev=PageView&noscript=1" />
         </noscript>
       </head>
       <body className="antialiased font-sans">{children}</body>
