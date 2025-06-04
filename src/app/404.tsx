6c7cbdbbac20df64860c19e1fcadfcafeeed5c22
Wisnu Dinar Pangestu <canvatron@gmail.com>
2025-06-04 11:31:38 +0700

    Update project: fitur, konten, dan custom 404 page

---
+++ src/app/404.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: 40 }}>
+      <h1>404 - Halaman Tidak Ditemukan</h1>
+      <p>Maaf, halaman yang kamu cari tidak tersedia.</p>
+    </div>
+  );
+} 
